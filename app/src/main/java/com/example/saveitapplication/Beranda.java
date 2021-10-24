64a495a65e641a8ad9d2f793dfc3edb080760c22
Rahadian Adikusuma <rahadian_98@yahoo.co.id>
2021-10-25 01:23:03 +0700

    all pages are now connected

---
+++ app/src/main/java/com/example/saveitapplication/Beranda.java
@@ -0,0 +1,46 @@
+package com.example.saveitapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class Beranda extends AppCompatActivity {
+
+    ImageButton button_pemasukan, button_pengeluaran, button_detail, button_setting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beranda);
+
+        button_pemasukan = findViewById(R.id.button_pemasukan);
+        button_pengeluaran = findViewById(R.id.button_pengeluaran);
+
+        button_pemasukan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Beranda.this, TambahPemasukan.class);
+                startActivity(intent);
+            }
+        });
+
+        button_pengeluaran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Beranda.this, DetailCashFlow.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Beranda.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/saveitapplication/MainActivity.java
+++ app/src/main/java/com/example/saveitapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.saveitapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     EditText username, password;
-    Button button_simpan_user, button_login;
+    Button button_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +22,7 @@ public class MainActivity extends AppCompatActivity {
         username = findViewById(R.id.textUser);
         password = findViewById(R.id.textPassword);
 
-        button_simpan_user = findViewById(R.id.button_save_user);
         button_login = findViewById(R.id.button_login);
-
-        button_simpan_user.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
-                databaseHelper.addUser(username.getText().toString(), password.getText().toString());
-            }
-        });
 
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,7 +34,9 @@ public class MainActivity extends AppCompatActivity {
                 boolean result = databaseHelper.checkUser(user, pass);
 
                 if (result) {
-                    Toast.makeText(MainActivity.this, "Logged In!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Beranda.class);
+                    startActivity(intent);
+                    finish();
                 }
                 else {
                     Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
