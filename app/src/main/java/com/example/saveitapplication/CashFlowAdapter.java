7155a9973c580b8c843a59242d2d860cbb0659c2
Rahadian Adikusuma <rahadian_98@yahoo.co.id>
2021-10-24 16:02:11 +0700

    recycler view read done

--- app/src/main/java/com/example/saveitapplication/CashFlowAdapter.java
+++ app/src/main/java/com/example/saveitapplication/CashFlowAdapter.java
@@ -1,31 +1,58 @@
 package com.example.saveitapplication;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class CashFlowAdapter extends RecyclerView.Adapter<CashFlowAdapter.MyViewHolder> {
+
+    private Context context;
+    private ArrayList nominal, keterangan, tanggal;
+
+    CashFlowAdapter(Context context, ArrayList nominal, ArrayList keterangan, ArrayList tanggal) {
+        this.context = context;
+        this.nominal = nominal;
+        this.keterangan = keterangan;
+        this.tanggal = tanggal;
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.cash_flow_layout, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.txt_nominal.setText(String.valueOf(nominal.get(position)));
+        holder.txt_keterangan.setText(String.valueOf(keterangan.get(position)));
+        holder.txt_tanggal.setText(String.valueOf(tanggal.get(position)));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return nominal.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView txt_nominal, txt_keterangan, txt_tanggal;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            txt_nominal = itemView.findViewById(R.id.text_nominal);
+            txt_keterangan = itemView.findViewById(R.id.text_keterangan);
+            txt_tanggal = itemView.findViewById(R.id.text_tanggal);
         }
     }
 }
--- app/src/main/java/com/example/saveitapplication/DetailCashFlow.java
+++ app/src/main/java/com/example/saveitapplication/DetailCashFlow.java
@@ -1,6 +1,7 @@
 package com.example.saveitapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.database.Cursor;
@@ -15,6 +16,7 @@ public class DetailCashFlow extends AppCompatActivity {
 
     DatabaseHelper databaseHelper;
     ArrayList<String> tanggal, nominal, tipe, keterangan;
+    CashFlowAdapter cashFlowAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +28,12 @@ public class DetailCashFlow extends AppCompatActivity {
         databaseHelper = new DatabaseHelper(DetailCashFlow.this);
         tanggal = new ArrayList<>();
         nominal = new ArrayList<>();
-        tipe = new ArrayList<>();
+        //tipe = new ArrayList<>();
         keterangan = new ArrayList<>();
+
+        cashFlowAdapter = new CashFlowAdapter(DetailCashFlow.this, nominal, tanggal, keterangan);
+        recyclerView.setAdapter(cashFlowAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(DetailCashFlow.this));
 
         storeDataInArrays();
     }
@@ -39,10 +45,10 @@ public class DetailCashFlow extends AppCompatActivity {
         }
         else {
             while (cursor.moveToNext()) {
-                tanggal.add(cursor.getString(0));
-                nominal.add(cursor.getString(1));
-                tipe.add(cursor.getString(2));
-                keterangan.add(cursor.getString(3));
+                tanggal.add(cursor.getString(1));
+                nominal.add(cursor.getString(2));
+                //tipe.add(cursor.getString(2));
+                keterangan.add(cursor.getString(4));
             }
         }
     }
